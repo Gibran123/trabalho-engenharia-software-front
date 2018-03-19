2e5d78034b1deeb1ef65dce78e4dd41cf606977f
Gibran Tavares <gibran@edu.unifor.br>
2018-03-19 13:24:24 -0300

    Criando a arquitetura do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
