157a1b76cab4b2f15a1f479a02709312c44b1021
Gibran Tavares <gibran@edu.unifor.br>
2018-04-01 12:02:43 -0300

    Colocando o menu no header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     HeaderComponent
+  ],
+  imports: [
+    RouterModule
   ],
   exports: [
     HeaderComponent
